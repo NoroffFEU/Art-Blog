5c9e694a84ffa37676858eca2407c6fc22ad6238
juliehabb <julie.habbestad@hotmail.com>
2024-05-05 18:48:40 +0200

    Worked on css on all pages.

---
+++ js/nav.js
@@ -0,0 +1,9 @@
+
+const navBars = document.getElementById("navBars");
+const navLinks = document.getElementById("navLinks");
+
+
+navBars.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+
+})
