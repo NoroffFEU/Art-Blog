c2bddb8cdfa089b8f4eaa4c386e5dd89acf817a5
juliehabb <julie.habbestad@hotmail.com>
2024-05-28 15:30:52 +0200

    Default image when no url

--- js/handlers/imagePreview.mjs
+++ js/handlers/imagePreview.mjs
@@ -1,13 +1,14 @@
 
-export function setImagePreview() {
+  export function setImagePreview() {
     const mediaInput = document.querySelector("#mediaField");
     const previewImage = document.querySelector("#pImage");
-  
+    const defaultImageUrl = "../images/No_Image.jpg"; 
+
     const updatePreview = () => {
         const url = mediaInput.value;
-        previewImage.src = url;
+        previewImage.src = url ? url : defaultImageUrl;
     };
-  
+
     if (mediaInput && previewImage) {
         // Update preview when input value changes
         mediaInput.addEventListener('input', updatePreview);
@@ -20,7 +21,7 @@ export function setImagePreview() {
     } else {
         console.error("Media input or preview image element not found");
     }
-  }
+}
 
 
 
