dd2a9245f3d0625fda3e028962510ac42c777266
juliehabb <julie.habbestad@hotmail.com>
2024-05-28 16:52:00 +0200

    Updated forms

--- js/index.mjs
+++ js/index.mjs
@@ -1,10 +1,5 @@
 import * as listeners from "./handlers/index.mjs";
-import * as postMethods from "./api/posts/index.mjs";
-import * as templates from "./templates/index.mjs";
 import { openNavMobile } from "./nav.mjs";
-
-
-
 
 
 const path = location.pathname;
@@ -13,9 +8,6 @@ if (path.startsWith("/account/register.html")) {
   listeners.setRegisterFormListener();
 } else if (path.startsWith("/account/login.html")) {
   listeners.setLoginFormListener();
-} else {
-  console.log("No matching path found for setting listeners");
-}
-
+} 
 
 openNavMobile();
